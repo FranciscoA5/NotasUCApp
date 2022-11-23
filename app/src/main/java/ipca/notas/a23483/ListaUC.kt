f5ccc58a664cc454f94bbf863d4ffe90946d4ec6
TostaMista30 <f-infsec03@hotmail.com>
2022-11-23 01:23:15 +0000

    first commit

---
+++ app/src/main/java/ipca/notas/a23483/ListaUC.kt
@@ -0,0 +1,11 @@
+package ipca.notas.a23483
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaUC : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_uc)
+    }
+}
---
+++ app/src/main/java/ipca/notas/a23483/MainActivity.kt
@@ -0,0 +1,16 @@
+package ipca.notas.a23483
+
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.BaseAdapter
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val notas = arrayListOf<CriarNotas>()
+        //val notasAdapter = ItemsAdapter()
+
+    }
+}
