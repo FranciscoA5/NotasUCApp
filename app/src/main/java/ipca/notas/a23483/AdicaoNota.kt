35c4a6b57ff5db7e9f3e3e9acc2e93b5545d24d0
TostaMista30 <f-infsec03@hotmail.com>
2022-11-23 01:23:15 +0000

    first commit

---
+++ app/src/main/java/ipca/notas/a23483/AdicaoNota.kt
@@ -0,0 +1,11 @@
+package ipca.notas.a23483
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdicaoNota : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicao_nota)
+    }
+}
---
+++ app/src/main/java/ipca/notas/a23483/CriarNotas.kt
@@ -0,0 +1,13 @@
+package ipca.notas.a23483
+
+class CriarNotas {
+    var nota: Double? = null
+    var uc: String? = null
+    var ano: Int? = null
+
+    constructor(nota : Double, uc : String, ano : Int){
+        this.nota = nota
+        this.uc = uc
+        this.ano = ano
+    }
+}
