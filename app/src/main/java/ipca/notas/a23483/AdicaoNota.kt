943d39c2f05e29ce1cf01accfa51203838f3eeaa
TostaMista30 <f-infsec03@hotmail.com>
2022-11-27 00:51:11 +0000

    Second commit

--- app/src/main/java/ipca/notas/a23483/AdicaoNota.kt
+++ app/src/main/java/ipca/notas/a23483/AdicaoNota.kt
@@ -1,11 +1,36 @@
 package ipca.notas.a23483
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class AdicaoNota : AppCompatActivity() {
+
+    var nota : Double? = null
+    var ano  : Int? = null
+    var uc : String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_adicao_nota)
+
+        nota = intent.getDoubleExtra("nota", 0.0)
+        ano = intent.getIntExtra("ano", 0)
+        uc = intent.getStringExtra("uc")
+
+        findViewById<EditText>(R.id.nota).setText(nota.toString())
+        findViewById<EditText>(R.id.ano).setText(ano.toString())
+        findViewById<EditText>(R.id.curso).setText(uc)
+
+        findViewById<Button>(R.id.buttonSave).setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("nota",findViewById<EditText>(R.id.nota).text.toString().toDouble())
+            intent.putExtra("ano",findViewById<EditText>(R.id.ano).text.toString().toInt() )
+            intent.putExtra("uc",findViewById<EditText>(R.id.curso).text.toString())
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/ipca/notas/a23483/MainActivity.kt
+++ app/src/main/java/ipca/notas/a23483/MainActivity.kt
@@ -1,16 +1,92 @@
 package ipca.notas.a23483
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.BaseAdapter
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ListView
+import android.widget.TextView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    var notas = arrayListOf<CriarNotas>()
+    val notasAdapter = NotasAdapter()
+
+    var resultLauncher : ActivityResultLauncher<Intent>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val notas = arrayListOf<CriarNotas>()
-        //val notasAdapter = ItemsAdapter()
+        val buttonAdd = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+
+        val nota1 = CriarNotas(12.7, "math", 1)
+        notas.add(nota1)
+
+        val notasview = findViewById<ListView>(R.id.notasvista)
+        notasview.adapter = notasAdapter
+
+        resultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()){
+            if (it.resultCode == Activity.RESULT_OK){
+
+                val nota = it.data?.getDoubleExtra("nota", 0.0)
+                val ano = it.data?.getIntExtra("ano",0)
+                val uc = it.data?.getStringExtra("uc")
+                notas.add(CriarNotas(nota?:0.0,uc?:"",ano?:0))
+
+                notasAdapter.notifyDataSetChanged()
+
+            }
+        }
+
+        buttonAdd.setOnClickListener {
+            resultLauncher?.launch(Intent(this@MainActivity,AdicaoNota::class.java))
+        }
+    }
+
+
+    inner class NotasAdapter : BaseAdapter() {
+        override fun getCount(): Int {
+            return notas.size
+        }
+
+        override fun getItem(p0: Int): Any {
+            return notas[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return 0
+        }
+
+
+        override fun getView(p0: Int, p1: View?, p2: ViewGroup?): View {
+            val rootView = layoutInflater.inflate(R.layout.row_item, p2, false)
+
+            val nota = rootView.findViewById<TextView>(R.id.gradeView)
+            val ano = rootView.findViewById<TextView>(R.id.anoView)
+            val curso = rootView.findViewById<TextView>(R.id.cursoView)
+
+            nota.text = notas[p0].nota.toString()
+            ano.text = notas[p0].ano.toString()
+            curso.text = notas[p0].uc
+
+            val bConstrain = rootView.findViewById<ConstraintLayout>(R.id.buttonConstrain)
+            bConstrain.setOnClickListener {
+                val intent = Intent(this@MainActivity, AdicaoNota::class.java)
+                startActivity(intent)
+            }
+
+            return rootView
+        }
 
     }
 }
